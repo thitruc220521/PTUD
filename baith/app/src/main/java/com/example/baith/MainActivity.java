5a5082b8dd9fa61b159f94df626f3447a0053eb1
thitruc220521 <thantruc8404@gmail.com>
2025-03-20 21:47:12 +0700

    initial commit

---
+++ baith/app/src/main/java/com/example/baith/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.baith;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Log log;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        log.d("MainActivity", "baith");
+    }
+}
